a35aac1684e40938e9ccf7f902987d623f7cc717
HPCenturionXV <145242952+HPCenturionXV@users.noreply.github.com>
2023-09-18 11:02:19 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b><u>Exercise 2 : Create markdown cell with Title of the Notebook<u><b>
+
+# # Data Science Tools and Ecosystem
+
+# **<u>Exercise 3 : Create a markdown cell for an introduction<u>**
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **<u>Exercise 10 - Insert a markdown cell to list Objectives<u>**
+
+# **Objectives:**
+# 
+# - List popular languages for data science
+# - List commonly used libraries by data scientists
+# - List popular data science tools
+# - Overview and introduction to repositories such as Github
+# - Introduction to different development environments such as RStudio, JupyterLab, VisualStudio
+# 
+
+# **<u>Exercise 4 : Create a markdown cell to list data science languages<u>**
+
+# Some of the popular languages Data Scientists use are:
+# 
+# 1. Python
+# 1. R
+# 1. Julia
+# 1. Java
+# 1. Scala
+# 
+
+# **<u>Exercise 5 : Create a markdown cell to list data science libraries<u>**
+
+# Some of the most commonly used Libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 1. Matplotlib.pyplot
+# 1. Numpy
+# 1. TensorFlow
+# 1. Scipy
+# 
+
+# **<u>Exercise 6 : Create a markdown cell with a table of Data Science tools<u>**
+
+# | Data Science Tools| 
+# | :---------------- | 
+# | Apache Spark      | 
+# | Apache Hadoop     | 
+# | JupyterLab        |  
+# | R studio          |  
+# | TensorFlow        |
+
+# **<u>Exercise 7 : Create a markdown cell introducing arithmetic expression examples<u>**
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# **<u>Exercise 8 : Create a code cell to multiply and add numbers<u>**
+
+# In[8]:
+
+
+x=3
+y=4
+z=5
+#This is a simple arithmetic expression to multiply then add integers
+Answer=(x*y)+z
+print(Answer)
+
+
+# **<u>Exercise 9 : Create a code cell to convert minutes to hours<u>**
+
+# In[9]:
+
+
+t=60
+Minutes=200
+#This will convert 200 minutes into hours by dividing by 60
+Hours= Minutes/t
+#round answer off to 2 decimal places 
+print (round(Hours,2))
+
+
+# **<u>Exercise 10 : Insert a markdown cell to list Objectives<u>**
+
+# Below the introduction cell created in Exercise 3
+
+# **<u>Exercise 11 : Create a markdown cell to indicate the Author's name<u>**
+
+# ## Author
+# 
+# Hetarth Patel
+
+# In[ ]:
+
+
+
+
